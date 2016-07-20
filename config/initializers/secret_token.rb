75d3b3f18144d2cf66379ffc38238e88c81530b3
Ubuntu <ubuntu@ip-172-31-36-40.us-west-2.compute.internal>
2016-07-20 06:07:30 +0000

    Rails new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Whelphack::Application.config.secret_key_base = 'c22d099d0e0ad3019dc9b51c342cc91c0c3c8da9fb56342ef285188716131e66b980e69c1c874c5fc6c72981f87f846317be8f9289015513eb40a093c133b77a'
